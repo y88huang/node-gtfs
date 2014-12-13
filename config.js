f17565b09b58b3681561259b23bb13b7bb5f5cf2
Yundi Huang <kenkaka.hyd@gmail.com>
2014-12-12 22:47:52 -0500

    fix url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'test'){
 }
 
 module.exports = {
-  mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gtfs'
+  mongo_url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/gtfs'
   , agencies: [
     /* Put agency_key names from gtfs-data-exchange.com.
      Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
