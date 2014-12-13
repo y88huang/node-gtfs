e95036a96aaf254c9316ba753745e461c72ab11f
Yundi Huang <kenkaka.hyd@gmail.com>
2014-12-12 22:12:55 -0500

    update config file

--- config.js
+++ config.js
@@ -8,12 +8,6 @@ module.exports = {
   , agencies: [
     /* Put agency_key names from gtfs-data-exchange.com.
      Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-    'alamedaoakland-ferry'
-    , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
-    , 'ac-transit'
-    , 'county-connection'
-    , 'san-francisco-municipal-transportation-agency'
-    , 'bay-area-rapid-transit'
-    , 'golden-gate-ferry'
+    { agency_key: 'grand-river-transit', url: 'http://www.gtfs-data-exchange.com/agency/grand-river-transit/latest.zip'}
   ]
 };
